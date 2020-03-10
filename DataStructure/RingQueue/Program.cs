1f117b65f12aad5c026428d2ab0273a3e5343b26
llj <llj@DESKTOP-FBADRVD>
2020-03-10 09:54:25 +0800

    环形队列

---
+++ DataStructure/RingQueue/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RingQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int> { 1, 2, 3 };
+            List<int> b = new List<int> { 2, 3, 3 };
+            a = a.Except(b).ToList();
+        }
+    }
+
+    /// <summary>
+    /// 环形队列
+    /// </summary>
+    public class RingQueue
+    {
+        /// <summary>
+        /// 初始值为0 是队列的头
+        /// </summary>
+        public int front { get; set; }
+
+        /// <summary>
+        /// 初始值为0 是队列的尾巴
+        /// </summary>
+        public int rear { get; set; }
+
+        /// <summary>
+        /// 创建队列
+        /// </summary>
+        public int[] arr { get; set; }
+
+        /// <summary>
+        /// 初始化队列构造器
+        /// </summary>
+        /// <param name="maxSize"></param>
+        public RingQueue(int maxSize) 
+        {
+            arr = new int[maxSize];
+        }
+
+        /// <summary>
+        /// 队列是否为空
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return front == rear?false:true;
+        }
+
+        /// <summary>
+        /// 队列有效值数量
+        /// </summary>
+        public void ExistSise() 
+        {
+            //if () 
+            //{
+            
+            //}
+        }
+    }
+}
---
+++ DataStructure/SingleLinkedList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleLinkedListDamo
+{
+
+    class Program
+    {
+
+        HeroNode head = new HeroNode(0, ""); //头节点、头指针    
+        static void Main(string[] args)
+        {
+            SingleLinkedList singleLinkedList = new SingleLinkedList();
+            HeroNode hero1 = new HeroNode(1, "孙悟空");
+            HeroNode hero2 = new HeroNode(2, "猪八戒");
+            HeroNode hero3 = new HeroNode(3, "沙和尚");
+            singleLinkedList.AddLinkedList(hero1);
+            singleLinkedList.AddLinkedList(hero3);
+            singleLinkedList.AddLinkedList(hero2);
+            singleLinkedList.ListLinkedList();
+            Console.ReadKey();
+        }
+    }
+    class SingleLinkedList
+    {
+        private HeroNode head = new HeroNode(0, "");
+
+        /// <summary>
+        /// 添加链表
+        /// </summary>
+        /// <param name="heroNode"></param>
+        public void AddLinkedList(HeroNode heroNode)
+        {
+            var temp = head;
+            while (true)
+            {
+                if (temp.Next == null)
+                    break;
+                temp = temp.Next;             
+            }
+            temp.Next = heroNode;
+        }
+
+        /// <summary>
+        /// 遍历链表
+        /// </summary>
+        /// <param name="heroNode"></param>
+        public void ListLinkedList()
+        {
+            var temp = head;
+            while (true)
+            {
+                if (temp.Next == null)
+                    break;
+                temp = temp.Next;
+            }
+          
+        }
+    }
+
+    class HeroNode
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        public HeroNode Next { get; set; }
+        public HeroNode(int no, string name)
+        {
+            No = no;
+            Name = name;
+        }
+    }
+}
